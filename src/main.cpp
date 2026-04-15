6382132bcf75f77a8502413c684e8fed353f1354
avr_dude <creed_truman77@my.albemarle.edu>
2026-04-14 20:20:18 -0400

    Filling out the Datasheet (just dumb test code)

--- src/main.cpp
+++ src/main.cpp
@@ -24,9 +24,17 @@ int main(void){
 
 	initTimer2Millis();
 	initTimer1Servo50Hz();
+	//This is just test code for a datasheet I had to fill out.
 	testDrive(1700, 1300);
 	testDrive(1300, 1700);
 	testDrive(1700, 1700);
 	testDrive(1300, 1300);
+	testDrive(1500, 1700);
+	testDrive(1300, 1500);
+	testDrive(1500, 1500);
+	testDrive(1520, 1480);
+	testDrive(1540, 1460);
+	testDrive(1700, 1450);
+	testDrive(1550, 1300);
 	while(true);
 }
