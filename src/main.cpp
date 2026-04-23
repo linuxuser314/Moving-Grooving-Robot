b7cb2d06277f580d4cb99294cbc83daf26cc4f7f
avr_dude <creed_truman77@my.albemarle.edu>
2026-04-22 22:52:00 -0400

    Week 13 Assignment 3: Simple movement functions

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,31 @@
 constexpr uint8_t TONE_PIN = 2;
 uint16_t melody[] = {1047, 1174, 1319, 1397, 1568, 1760, 1976, 2093};
 
+
+//drive and stop were defined in myiolib.h.
+
+void forward(uint16_t ms){
+	drive(100, 100);
+	myDelay(ms);
+	stop();
+}
+void backward(uint16_t ms){
+	drive(-100, -100);
+	myDelay(ms);
+	stop();
+}
+void turnLeft(uint16_t ms){
+	drive(-25, 25);
+	myDelay(ms);
+	stop();
+}
+void turnRight(uint16_t ms){
+	drive(25, -25);
+	myDelay(ms);
+	stop();
+}
+
+
 int main(void){
 
 	initTimer2Millis();
@@ -20,17 +45,11 @@ int main(void){
 		myTone(melody[i], 350);
 		myDelay(25);
 	}
-	drive(100, 100);
-	myDelay(6000);
-	stop();
-	myDelay(2000);
-	drive(-25, 25);
-	myDelay(1000);
-	drive(25, -25);
-	myDelay(1000);
-	drive(-100, -100);
-	myDelay(6000);
-	stop();
+	forward(3000);
+	backward(3000);
+	turnLeft(3000);
+	turnRight(3000);
+	//stops automatically at the end of the duration
 
 	while(true){}
 }
